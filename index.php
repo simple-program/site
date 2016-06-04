6e388a0f2355f27343e0edf85175bbb04774ef0d
simple-program <n-cooper@outlook.com>
2016-06-04 19:05:05 +0300

    New file

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap 101 Template</title>
+
+  <link href="/bootstrap/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+  <div class="row">
+    <div class="color col-md-3 col-sm-3 col-xs-3 col-lg-3">.col-md-1</div>
+    <div class="colors col-md-9 col-sm-9 col-xs-9 col-lg-9">.col-md-1</div>
+  </div>
+  </div>
+  
+  <style>
+    .color {
+      background-color: red;
+    }
+    .colors {
+      background-color: blue;
+    }
+  </style>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
